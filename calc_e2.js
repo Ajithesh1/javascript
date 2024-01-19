6a89836cf07c07e8b7c145f8b07f7bae86f84ace
Ajithesh1 <109501973+Ajithesh1@users.noreply.github.com>
2024-01-19 16:50:14 +0530

    Add files via upload

---
+++ calc_e2.js
@@ -0,0 +1,12 @@
+exports.add=function add(x,y){
+    return x+y;
+}
+exports.sub=function add(x,y){
+    return x-y;
+}
+exports.mul=function add(x,y){
+    return x*y;
+}
+exports.div=function add(x,y){
+    return x/y;
+}
